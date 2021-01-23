a8b3259a52249211389b7c91e67255ca2615f9b6
faqihakih <faqihakih12@gmail.com>
2021-01-24 04:14:09 +0700

    4 - fix routing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mahasiswaController = require('./controller/controller_mhs')
 const app = express();
 
 // parse requests of content-type: application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
 
@@ -14,28 +14,28 @@ const port = 3000;
 
 
 app.get('/', (req, res) => {
-    let sql = "SELECT nama_mhs, kelas, nama_dosen, kode_mk, nama_mk FROM mahasiswa as mhs JOIN dosen as dsn ON mhs.dosen_wali = dsn.id JOIN matakuliah as mk ON dsn.pengampuh = mk.id";
-    let query = conn.query(sql, (err, result) => {
-        // !err ? result.forEach(element => {
-        //     res.json({
-        //         nama : element.nama_mhs,
-        //         kelas: element.kelas,
-        //         nama_dosen : element.nama_dosen,
-        //         kode_mk : element.kode_mk,
-        //         nama_mk : element.nama_mk
-        //     })
-        // }) : console.log("Query error");
-        res.json({
-            "message" : "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."
-        })
-        conn.end();
+    //    let sql = "SELECT nama_mhs, kelas, nama_dosen, kode_mk, nama_mk FROM mahasiswa as mhs JOIN dosen as dsn ON mhs.dosen_wali = dsn.id JOIN matakuliah as mk ON dsn.pengampuh = mk.id";
+    //  let query = conn.query(sql, (err, result) => {
+    // !err ? result.forEach(element => {
+    //     res.json({
+    //         nama : element.nama_mhs,
+    //         kelas: element.kelas,
+    //         nama_dosen : element.nama_dosen,
+    //         kode_mk : element.kode_mk,
+    //         nama_mk : element.nama_mk
+    //     })
+    // }) : console.log("Query error");
+    res.json({
+        "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."
     })
+    //    conn.end();
 })
 
 app.get("/mahasiswa", mahasiswaController.findAll);
 app.get("/mahasiswa/:mahasiswaId", mahasiswaController.findOne);
 app.post("/mahasiswa", mahasiswaController.create)
 
-app.listen(port, ()=>{
-    console.log("berjalan pada http://localhost:"+port);
+app.listen(port, () => {
+    console.log("berjalan pada http://localhost:" + port);
 })
+6
